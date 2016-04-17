177c7532067f3a6a37468acb2c956f585ed4793a
skingd <sking@mail.greenriver.edu>
2016-04-16 17:38:58 -0700

    File Structure Update

---
+++ depend/includes/dbcon.php
@@ -0,0 +1,20 @@
+<?php
+
+function readDatabase(){
+    
+    $servername = "localhost";
+$username = "tps_test_read";
+$password = "w8.~7DyCX]$8";
+    
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=tps_test", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn; 
+        }
+    catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage();
+        }
+}
+?>
